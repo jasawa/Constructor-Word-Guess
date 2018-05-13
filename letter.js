6a22f6ebc2f672415b425a6e75345a61547f6d24
Jane Asawa <asawajane@gmail.com>
2018-05-13 13:00:16 -0700

    draft of letter.js

--- letter.js
+++ letter.js
@@ -1,30 +1,50 @@
-
+//var placeholder = "_";
+//var guess = "q";
 // Constructor function creating letter objects
-function Letter(character, isGuessed, returnChar, updateLetter) {
+function Letter(character) {
     this.character = character;
     this.isGuessed = false;
+
     this.returnChar = function() {
         if (isGuessed === true) {
-            placeHolder = character;
+            placeholder = this.character;
+            console.log("placeholder " + placeholder);
         }
         else {
-            placeHolder = "_";
+            placeholder = "_";
+            console.log("placeholder " + placeholder);
         }
     };
-    this.updateLetter = function(character){
+
+    this.changeIsGuessed = function(character) {
         if (guess === character) {
             this.isGuessed = true;
+            console.log("---true----" + this.isGuessed);
         }
-    }
+        else {
+            console.log("----false---" + this.isGuessed);
+        }
+    };
+    
+    
+    /*this.printInfo = function() {
+        console.log("this.character is " + this.character);
+        console.log("this.isGuessed is " + this.isGuessed);
+        console.log("this.character is " + this.placeholder);
+        console.log("this.changeIsGuessed is " + this.isGuessed);
+        console.log("guess is " + guess);
+    };*/
 }
 
 // creates the printInfo method and applies it to all letter objects
-Letter.prototype.printInfo = function() {
+/*Letter.prototype.printInfo = function() {
 console.log("In letter.js, the character is " + this.character + "\nthe letter was guessed: " + this.isGuessed +
-"\nIn returnChar, the placeholder is : " + placeHolder);
+"\nIn returnChar, the placeholder is : " + placeholder);
 console.log("---------------");
-};
+}*/
+//var guess = "q";
+//var newLetter = new Letter("q");
+//newLetter.printInfo();
 
-Letter.printInfo();
-
+// exporting Letter to use in word.js
 module.exports = Letter;
